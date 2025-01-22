f39df28a86183eae89b8f4d4c7647938c2df7def
Jason Chen <jason@engineerforce.io>
2025-01-22 17:51:10 +0800

    Custom some data

--- frontend/app/lib/data.ts
+++ frontend/app/lib/data.ts
@@ -8,6 +8,7 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
+import { customers } from './placeholder-data';
 
 export async function fetchRevenue() {
   try {
@@ -15,9 +16,25 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
-    const data = await sql<Revenue>`SELECT * FROM revenue`;
+    // const data = await sql<Revenue>`SELECT * FROM revenue`;
+    const data = {
+      rows: [
+        { month: 'Jan', revenue: 2000 },
+        { month: 'Feb', revenue: 1800 },
+        { month: 'Mar', revenue: 2200 },
+        { month: 'Apr', revenue: 2500 },
+        { month: 'May', revenue: 2300 },
+        { month: 'Jun', revenue: 3200 },
+        { month: 'Jul', revenue: 3500 },
+        { month: 'Aug', revenue: 3700 },
+        { month: 'Sep', revenue: 2500 },
+        { month: 'Oct', revenue: 2800 },
+        { month: 'Nov', revenue: 3000 },
+        { month: 'Dec', revenue: 4800 },
+      ]
+    }
 
     // console.log('Data fetch completed after 3 seconds.');
 
@@ -30,12 +47,52 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
-    const data = await sql<LatestInvoiceRaw>`
-      SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
-      FROM invoices
-      JOIN customers ON invoices.customer_id = customers.id
-      ORDER BY invoices.date DESC
-      LIMIT 5`;
+    // const data = await sql<LatestInvoiceRaw>`
+    //   SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
+    //   FROM invoices
+    //   JOIN customers ON invoices.customer_id = customers.id
+    //   ORDER BY invoices.date DESC
+    //   LIMIT 5`;
+
+    const data = {
+      rows: [
+        {
+          name: 'Evil Rabbit',
+          email: 'evil@rabbit.com',
+          image_url: '/customers/evil-rabbit.png',
+          amount: 15795,
+          id: 'd6e15727-9fe1-4961-8c5b-ea44a9bd81aa',
+        },
+        {
+          name: 'Delba de Oliveira',
+          email: 'delba@oliveira.com',
+          image_url: '/customers/delba-de-oliveira.png',
+          amount: 20569,
+          id: '3958dc9e-712f-4377-85e9-fec4b6a6442a'
+        },
+        {
+          name: 'Amy Burns',
+          email: 'amy@burns.com',
+          image_url: '/customers/amy-burns.png',
+          amount: 30,
+          id: 'CC27C14A-0ACF-4F4A-A6C9-D45682C144B9',
+        },
+        {
+          name: 'Michael Novotny',
+          email: 'michael@novotny.com',
+          image_url: '/customers/michael-novotny.png',
+          amount: 44800,
+          id: '76d65c26-f784-44a2-ac19-586678f7c2f2',
+        },
+        {
+          name: 'Balazs Orban',
+          email: 'balazs@orban.com',
+          image_url: '/customers/balazs-orban.png',
+          amount: 5000,
+          id: '13D07535-C59E-4157-A011-F8D2EF4E0CBB',
+        },
+      ]
+    }
 
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
